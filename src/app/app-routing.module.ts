cec08a905e31830a38ec71d3cee635ab5147bc8f
rahulmistari2141 <rahulmistari2141@gmail.com>
2023-09-14 18:59:59 +0530

    install file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'public', loadChildren: () => import('./layouts/public/public.module').then(m => m.PublicModule) }, { path: 'secure', loadChildren: () => import('./layouts/secure/secure.module').then(m => m.SecureModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
